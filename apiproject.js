00c7c70ad91ab88987c72df062857c5c3ec55199
jmercado1 <125997850+jmercado1@users.noreply.github.com>
2023-10-08 21:10:55 -0500

    Add files via upload

---
+++ apiproject.js
@@ -0,0 +1,29 @@
+[
+    {
+        "question": "Dogs can be trained to detect epileptic seizures in humans.",
+        "options": ["True", "False"],
+        "correctAnswer": "True"
+    },
+    {
+        "question": "A Border Collie named Chaser has learned the names of 1,022 toys, and can retrieve each by name.",
+        "options": ["True", "False"],
+        "correctAnswer": "True"
+    },
+    {
+        "question": "Hyenas aren't actually dogs. They are more closely related to cats.",
+        "options": ["True", "False"],
+        "correctAnswer": "True"
+    },
+    {
+        "question": "The famous nickname, mans best friend, is used in many other languages as well as English.",
+        "options": ["True", "False"],
+        "correctAnswer": "True"
+    },
+    {
+        "question": "Dogs dont go into menopause and will continue to go on heat indefinitely unless they are spayed.",
+        "options": ["True", "False"],
+        "correctAnswer": "True"
+
+        <script src="apiproject.html"></script>
+    }
+]
